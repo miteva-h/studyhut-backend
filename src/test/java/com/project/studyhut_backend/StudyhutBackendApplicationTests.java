1710419741718aee678e68bb6a39a0a905085f7a
miteva-h <hmiteva7@gmail.com>
2023-04-11 18:05:59 +0200

    First commit

---
+++ src/test/java/com/project/studyhut_backend/StudyhutBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.studyhut_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyhutBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
