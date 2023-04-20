ebfa1e57ca60cc6602100e14cb02869bc8251e7e
marpost5 <mpostolovska15@gmail.com>
2023-04-20 20:33:06 +0200

    Implementation of spring security. Did a refactoring with login and register methods.

--- src/main/java/com/project/studyhut_backend/service/impl/UserServiceImpl.java
+++ src/main/java/com/project/studyhut_backend/service/impl/UserServiceImpl.java
@@ -1,39 +1,46 @@
 package com.project.studyhut_backend.service.impl;
 
-import com.project.studyhut_backend.exception.InvalidArgumentsException;
-import com.project.studyhut_backend.exception.InvalidUsernameOrPasswordException;
-import com.project.studyhut_backend.exception.PasswordsDoNotMatchException;
-import com.project.studyhut_backend.exception.UsernameAlreadyExistsException;
+import com.project.studyhut_backend.exception.*;
 import com.project.studyhut_backend.model.Role;
 import com.project.studyhut_backend.model.User;
 import com.project.studyhut_backend.repository.UserRepository;
 import com.project.studyhut_backend.service.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+    @Override
+    public User login(String username, String password) {
+        if (username==null || username.isEmpty() || password==null || password.isEmpty()) {
+            throw new InvalidArgumentsException();
+        }
+        return userRepository.findByUsernameAndPassword(username,
+                password).orElseThrow(InvalidUserCredentialsException::new);
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return (UserDetails) userRepository.findByUsername(username).orElseThrow(()->new UsernameNotFoundException(username));
     }
 
     @Override
-    public User register(String email, String username, String name, String password, String repeatPassword, Role role) {
-        if (username==null || username.isEmpty()  || password==null || password.isEmpty())
+    public User register(String email,String username, String password, String repeatPassword, String name, Role role) {
+        if (email==null || email.isEmpty() ||username==null || username.isEmpty()  || password==null || password.isEmpty())
             throw new InvalidUsernameOrPasswordException();
         if (!password.equals(repeatPassword))
             throw new PasswordsDoNotMatchException();
-        if(this.userRepository.findByUsername(username)!=null)
+        if(this.userRepository.findByUsername(username).isPresent())
             throw new UsernameAlreadyExistsException(username);
-        User user = new User(email, username,name,password,role);
+        User user = new User(email,username,name,passwordEncoder.encode(password),role);
         return userRepository.save(user);
-    }
-
-    @Override
-    public User login(String username, String password) {
-            if (username==null || username.isEmpty() || password==null || password.isEmpty()) {
-                throw new InvalidArgumentsException();
-            }
-            return userRepository.findByUsernameAndPassword(username,
-                    password);
     }
 }
